d5a86698e920778fc1f36451215e837db738c159
Alexander Matyasko <alexander.matyasko@gmail.com>
2017-09-08 18:21:06 +0800

    Use melpa org-mime (it's removed from org-plus-contrib)

--- layers/+emacs/org/packages.el
+++ layers/+emacs/org/packages.el
@@ -24,6 +24,7 @@
     (org :location built-in)
     (org-agenda :location built-in)
     org-download
+    ;; org-mime is installed by `org-plus-contrib'
     org-mime
     org-pomodoro
     org-present
