1d05aadc00358466830bb0f279a5508b0d3c751d
Alexander Matyasko <alexander.matyasko@gmail.com>
2018-05-30 16:37:13 +0800

    Update epub layer
    Move nov-places to spacemacs cache directory

---
+++ layers/+readers/epub/config.el
@@ -0,0 +1,13 @@
+;;; config.el --- epub Layer config File for Spacemacs
+;;
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;;
+;; Author: Alexander Matyasko <alexander.matyasko@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defvar epub-nov-text-width 80
+  "Width of eBook text. Defaults to 80. ")
